e0bc87def27984bd5805f3962e863fee3f810414
Lavlena <lavrinovic.lena@gmail.com>
2023-02-08 15:46:16 +0200

    created register form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,38 @@
 const Register = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.username.value);
+        console.log(e.target.email.value);
+        console.log(e.target.password.value);
+        console.log(e.target.passwordrepeat.value);
+      }
+    
+
     return(
         <>
         <h1> Register </h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label> User Name
+                    <input type="text"
+                           name="username" />
+                </label> Email
+                <label>
+                    <input type="email"
+                           name="email" />
+                </label>
+                <label> Passwopd
+                    <input type="password"
+                           name="password" />
+                </label>
+                <label> Repeat Password
+                    <input type="password"
+                           name="passwordrepeat" />
+                </label>
+                <button type="submit"> Sign Up </button>
+            </form>
+        </div>
         </>
     )
 }
