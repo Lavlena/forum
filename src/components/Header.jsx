d2edf9741f7e7cbe7133148dffc8f157712cb94b
Lavlena <lavrinovic.lena@gmail.com>
2023-02-08 12:05:19 +0200

    header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return(
         <header>
-             <h1> Hello </h1>
+                 <h1> Hello </h1>
+                <div className="menu">
+                    <Link to={'/'}> Home </Link>
+                    <Link to={'/answers'}> Answers </Link>
+                    <Link to={'/newQuestion'}> New Question </Link>
+                    <Link to={'/newAnswer'}> New Answer </Link>
+                </div>
+                    <div className="logreg">
+                        <Link to={'/login'}> Log in </Link>
+                        <Link to={'/register'}> Register </Link>
+                    </div>
         </header>
     )
 }
