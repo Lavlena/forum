68fdb352f3855f12fa5cb455fc6db236c2cc2c72
Lavlena <lavrinovic.lena@gmail.com>
2023-02-08 16:30:57 +0200

    some changes in register, created newQuestion form

--- src/components/NewQuestion.jsx
+++ src/components/NewQuestion.jsx
@@ -1,7 +1,29 @@
 const NewQuestion = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.title.value);
+        console.log(e.target.question.value);
+      }
+    
+
     return(
         <>
         <h1> New Question </h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label> Title
+                    <input type="text"
+                           name="title"/>
+                </label>
+                <label> Question
+                  <textarea name="question" 
+                            cols="30" 
+                            rows="10"></textarea>
+                </label>
+                <button type="submit"> Submit </button>
+            </form>
+        </div>
         </>
     )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -6,6 +6,7 @@ const Register = () => {
         console.log(e.target.email.value);
         console.log(e.target.password.value);
         console.log(e.target.passwordrepeat.value);
+        console.log(e.target.avatar.value);
       }
     
 
@@ -29,6 +30,10 @@ const Register = () => {
                 <label> Repeat Password
                     <input type="password"
                            name="passwordrepeat" />
+                </label>
+                <label> User Avatar
+                    <input type="url"
+                           name="avatar" />
                 </label>
                 <button type="submit"> Sign Up </button>
             </form>
