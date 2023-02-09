2bd7aa8a7711853144e7deaee47e693d807a09f7
Lavlena <lavrinovic.lena@gmail.com>
2023-02-09 10:30:51 +0200

    changes json,index, created UserContext,

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import { createContext } from "react";
+import { useState } from "react";
+
+const UserContext = createContext();
+
+const UserProvider = ({children}) =>{
+const [users, setUsers] = useState (null);
+const [isLoggedIn, setIsLoggedIn] = useState (false);
+
+//STATE FUNCTION
+// const addUser = (newUser) => {
+// setUsers ([...users,newUser]);  
+//}
+//CRUD FUNCTIONS
+ const postUser = (newUser) => {
+    fetch ('http://localhost:5000/users', {
+        method: 'POST',
+        headers: {'Content-Type':"aplication/json"},
+        body: JSON.stringify(newUser),
+    })
+    .then(res => res.json());
+ }
+
+ const getUsers = async () => {
+    const data = await fetch ('http://localhost:5000/users')
+    .then(res => res.json());
+    setUsers(data);
+ }
+
+ useEffect(() => {
+    getUsers();
+ }, []);
+
+    return(
+       
+        < UserContext.Provider
+        value={{
+            users,
+            setUsers,
+            isLoggedIn,
+            setIsLoggedIn,
+            postUser
+            
+        }}>
+        {children}
+        </ UserContext.Provider>
+    )
+}
+
+export {UserProvider};
+export default UserContext;
